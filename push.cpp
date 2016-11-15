b4ec722025637eb7c8df9813072e037e6408b63b
anish1992 <anishkum92@gmail.com>
2016-11-14 16:26:25 -0800

    new server upload program with layered quarry

--- push.cpp
+++ push.cpp
@@ -1,10 +1,42 @@
 #include<iostream>
 #include<fstream>
+#include<string>
 using namespace std;
 
+char *AddStr(string a, string b, string c);
+
 int main(void)  {
-	system("scp /Applications/XAMPP/xamppfiles/htdocs/anish/images/* anish@unixlab.sfsu.edu:/unixlab/anish/public_html/images/");
-	system("scp /Applications/XAMPP/xamppfiles/htdocs/anish/css/* anish@unixlab.sfsu.edu:/unixlab/anish/public_html/css/");
-	system("scp /Applications/XAMPP/xamppfiles/htdocs/anish/* anish@unixlab.sfsu.edu:/unixlab/anish/public_html/");
+    
+    string serve, quarry;
+    cout<< "Enter server <username@server>"<<endl;
+    cin>>serve;
+    cout<< "what would tou like to push\n[0] - images [1] - css [2] - index"<<endl;
+    cin>>quarry;
+    
+    if (quarry[0] == 'y')   {
+        cout<<"pushing to server - "<< serve <<endl;
+        system(AddStr("scp /Applications/XAMPP/xamppfiles/htdocs/anish/images/* ",serve,":/unixlab/anish/public_html/images/"));
+    }
+    if (quarry[1] == 'y')   {
+        cout<<"pushing to server - "<< serve <<endl;
+        system(AddStr("scp /Applications/XAMPP/xamppfiles/htdocs/anish/css/* ",serve,":/unixlab/anish/public_html/css/"));
+    }
+    if (quarry[2] == 'y')   {
+        cout<<"pushing to server - "<< serve <<endl;
+        system(AddStr("scp /Applications/XAMPP/xamppfiles/htdocs/anish/* ",serve,":/unixlab/anish/public_html/"));
+    }
+		
     return 0;
+}
+
+char *AddStr(string a, string b, string c) {
+    
+    int i=0,j=0,k=0;
+    static char buffer[100];// pointer buffer
+    char *p = buffer;
+    while(a[i] != '\0')*p++ = a[i++];  // copy
+    while(b[j] != '\0')*p++ = b[j++];  // append
+    while(c[k] != '\0')*p++ = c[k++];  // append
+    return buffer;
+    
 }
